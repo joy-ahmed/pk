d8569b67683d73bb42c9581718a34e1f7224e115
Joy Ahmed <dev.joyahmed@gmail.com>
2023-10-08 12:15:22 +0600

    adds

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,5 @@ from store import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('products/', views.products, name="products"),
+    path('product/<str:product_id>', views.product, name="product"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import *
 
 # Create your views here.
@@ -9,3 +9,9 @@ def home(request):
 def products(request):
     products = Product.objects.all()
     return render(request,'products.html',{'products':products})
+
+
+def product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    print(product, product_id)
+    return render(request,'product.html',{'product':product})
